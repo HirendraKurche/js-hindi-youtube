50cfdcf1589b02128371a433d1a2a4de519537f3
hirendra <hirendrakurche423@gmail.com>
2024-07-30 19:54:22 +0530

    Create 1.js

---
+++ 10_classes_and_oop/1.js
@@ -0,0 +1,4 @@
+// 40th vedio screenshot
+// https://blog.logrocket.com/fetch-api-node-js/
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/fetch#specifications
+// https://fetch.spec.whatwg.org/#fetch-method
